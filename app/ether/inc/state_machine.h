97dd326196e5dd623e01241898d970d0acfadf53
PanBabinicz <babinicz007@icloud.com>
2024-06-08 12:34:58 +0200

    PROJECT-ETHER-REFACTOR: Add doxygen documentation for state_machine.

--- app/ether/inc/state_machine.h
+++ app/ether/inc/state_machine.h
@@ -4,68 +4,90 @@
 #include "ether.h"
 
 /** 
- * \param
- * \param
+ * \brief Initialize the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the initialization operation.
  */
 bme280_result_t state_machine_bme280_init(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Reset the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the reset operation.
  */
 bme280_result_t state_machine_bme280_reset(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Read the ID of the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the ID read operation.
  */
 bme280_result_t state_machine_bme280_id(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Set the BME280 sensor to force mode within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the force mode operation.
  */
 bme280_result_t state_machine_bme280_force_mode(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Measure humidity with the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the humidity measurement operation.
  */
 bme280_result_t state_machine_bme280_measure_humidity(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Measure temperature with the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the temperature measurement operation.
  */
 bme280_result_t state_machine_bme280_measure_temperature(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Measure pressure with the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the pressure measurement operation.
  */
 bme280_result_t state_machine_bme280_measure_pressure(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Get compensation data from the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the get compensation data operation.
  */
 bme280_result_t state_machine_bme280_get_compensation_data(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Compensate humidity measurement from the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the humidity compensation operation.
  */
 bme280_result_t state_machine_bme280_compensate_humidity(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Compensate temperature measurement from the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the temperature compensation operation.
  */
 bme280_result_t state_machine_bme280_compensate_temperature(ether_t *ether);
 
 /** 
- * \param
- * \param
+ * \brief Compensate pressure measurement from the BME280 sensor within the state machine.
+ * 
+ * \param[out]  ether: Pointer to the ether structure.
+ * \return      Result of the pressure compensation operation.
  */
 bme280_result_t state_machine_bme280_compensate_pressure(ether_t *ether);
 
